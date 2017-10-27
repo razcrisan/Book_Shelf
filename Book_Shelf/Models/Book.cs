8e1a594290ff67f2cf28580337fe8f4e03c39649
razcrisan <rcrisan13@gmail.com>
2017-10-27 10:10:13 -0400

    done editing

---
+++ Book_Shelf/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Book_Shelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        [MinLength(2), MaxLength(50)]
+        public string Title { get; set; }
+        [Display(Name = "Published Date")]
+        public DateTime PublishedDate { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        //[Required]
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category"), Display(Name = "Category")]
+        public int CategoryID {get; set;}
+        public virtual Category Category { get; set; }
+
+    }
+}
---
+++ Book_Shelf/Models/Book_ShelfContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Book_Shelf.Models
+{
+    public class Book_ShelfContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Book_ShelfContext() : base("name=Book_ShelfContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Book_Shelf.Models.Book> Books { get; set; }
+
+        public System.Data.Entity.DbSet<Book_Shelf.Models.Category> Categories { get; set; }
+    }
+}
